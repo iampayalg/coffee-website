74fb3a6db094a004f93f11c5538e601e9c42e89d
Payal Gulati <147704680+iampayalg@users.noreply.github.com>
2024-01-31 17:28:47 +0530

    home page pixel perfect

--- index.js
+++ index.js
@@ -5,21 +5,3 @@ function classToggle() {
   
   document.querySelector('.Navbar-Link-toggle')
     .addEventListener('click', classToggle);
-
-// const prefs = document.getElementsByClassName("preferences");
-// console.log(prefs);
-
-// Function to toggle the bar
-// function navbartoggle() {
-//   let x = document.getElementById("nav-items");
-//   if (x.style.display === "block") {
-//       x.style.display = "none";
-//   } else {
-//       x.style.display = "block";
-//   }
-// }
-
-// // Function to toggle the plus menu into minus
-// function myFunction(x) {
-//   x.classList.toggle("fa-solid fa-xmark");
-// }
